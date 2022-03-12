e78a05e7c46cb0c49df4d54d838db853917ba80b
NaomiMakiyama <luciane.makiyama@gmail.com>
2022-03-11 23:49:56 -0300

    tratamento PWA

--- PersonalBlog/Models/BlogPost.cs
+++ PersonalBlog/Models/BlogPost.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace PersonalBlog.Models
+﻿namespace PersonalBlog.Models
 {
     public class BlogPost
     {
